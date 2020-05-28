8ea74f851cc9fb3457c65ca3d6244db54154bb4a
ficasmica <54986618+ficasmica@users.noreply.github.com>
2020-05-28 16:17:45 +0200

    firstPush

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public Sprite frontSprite;
+    public Sprite backSprite;
+    private Animator anim;
+    private SpriteRenderer rend;
+    private Table table;
+    public bool isPaired = false;
+
+    void Awake(){
+        table = transform.parent.GetComponent<Table>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+        rend.sprite = backSprite;
+    }
+
+    void OnMouseDown(){
+        if (table.startTime == false){
+            table.startTime = true;
+        }
+        FlipCard();
+    }
+
+    void FlipCard(){
+        if (!table.canFlip){
+            return;
+        } else{
+            anim.SetBool("flip", true); // flip animacija
+            // storniraj slicice za check i start check
+            if (table.firstCard == null && table.secondCard == null){
+                table.firstCard = gameObject;
+            } else if (table.firstCard != null && table.secondCard == null){
+                table.secondCard = gameObject;
+                table.canFlip = false;
+                table.CheckCards();
+            }
+        }
+    }
+
+    public void FlipSprite(){
+        rend.sprite = frontSprite;
+    }
+
+    public void FlipBack(){
+        rend.sprite = backSprite;
+    }
+}
---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Table : MonoBehaviour
+{
+    public Sprite[] frontSprites;
+    public bool canFlip = true;
+    public GameObject firstCard;
+    public GameObject secondCard;
+    public float showTime;
+    private int pairsLeft;
+    private float stopwatch;
+    public bool startTime = false;
+
+    void Awake(){
+        SetCards();
+    }
+
+    void Start(){
+        stopwatch = 0f;
+    }
+
+    void Update(){
+        if (startTime == true){
+            stopwatch += Time.deltaTime;
+        }
+    }
+
+    void SetCards(){
+        List<Sprite> frontSpriteList = frontSprites.ToList<Sprite>();
+        List<Sprite> spriteList = frontSpriteList.Concat<Sprite>(frontSpriteList).ToList<Sprite>();
+
+        foreach (Transform child in transform){
+            int spriteIndex = Random.Range(0, spriteList.Count);
+            child.gameObject.GetComponent<Card>().frontSprite = spriteList[spriteIndex];
+            spriteList.Remove(spriteList[spriteIndex]);
+        }
+
+        pairsLeft = transform.childCount / 2;
+    }
+
+    public void CheckCards(){
+        if (firstCard.GetComponent<Card>().frontSprite == secondCard.GetComponent<Card>().frontSprite){ // ako je hit
+            pairsLeft--;
+            if (pairsLeft == 0){
+                startTime = false;
+                Debug.Log(stopwatch);
+            }
+            canFlip = true;
+            firstCard.GetComponent<Card>().isPaired = true;
+            secondCard.GetComponent<Card>().isPaired = true;
+            firstCard = null;
+            secondCard = null;
+        } else{
+            StartCoroutine(CloseCards());
+        }
+    }
+
+    private IEnumerator CloseCards(){
+        yield return new WaitForSeconds(showTime);
+        firstCard = null;
+        secondCard = null;
+        canFlip = true;
+        foreach (Transform child in transform){
+            if (child.gameObject.GetComponent<SpriteRenderer>().sprite == child.gameObject.GetComponent<Card>().frontSprite && child.gameObject.GetComponent<Card>().isPaired != true){
+                child.gameObject.GetComponent<Animator>().SetBool("flip", false);
+            }
+        }
+    }
+
+
+}
